bdf1e433206f94c114c14d150c7af301aaf76183
queuing4oranges <katjazenker@gmail.com>
2023-05-08 21:40:00 +0200

    finished 1st draft

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,63 @@
+import React, { useState } from "react";
+import { nanoid } from "nanoid";
+import text from "./data";
+
 const App = () => {
-  return <h2>Lorem Ipsum Starter</h2>;
+  const [counter, setCounter] = useState(1);
+  const [textArr, setTextArr] = useState([])
+
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    const newArr = text.slice(0, counter)
+    console.log(newArr)
+    setTextArr(newArr)
+    }
+
+  return ( 
+    <div className="container">
+      <h2 className="lorem-title">Lorem Ipsum Generator</h2>
+
+      <div className="form-container">
+        <form onSubmit={handleSubmit}>
+          <label htmlFor="count">Paragraphs</label>
+          <input 
+          type="number" 
+          id="count" 
+          name="count" 
+          min="1" 
+          max="8"
+          step="1"
+          value={counter}
+          onChange={(e)=>setCounter(e.target.value)} />
+          <button className="form-button" type="submit">GO!</button>
+        </form>
+
+        <div>
+          {textArr && 
+          
+          textArr.map((paragraph) => {
+            const id = nanoid()
+            console.log(id)
+
+            return (
+            <section key={id}>
+              <p>{paragraph}</p>
+            </section>
+            )
+          
+          })
+          
+          
+          }
+        </div>
+      </div>
+    </div>
+
+  )
+  
+  
+  
 };
 export default App;
